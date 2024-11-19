2a776867ed687da69f238a2fe08c6b0f266202c0
Zajfan <114745912+Zajfan@users.noreply.github.com>
2024-11-19 09:03:38 +0100

    update
    update the UIBase

--- VGE.Core/UI/Core/Base/Header Files/VGE.UIBase.h
+++ VGE.Core/UI/Core/Base/Header Files/VGE.UIBase.h
@@ -11,23 +11,6 @@ namespace VGE
 	public:
 		UIBase();
 		virtual ~UIBase();
-		virtual void Initialize();
-		virtual void Update();
-		virtual void Render();
-		virtual void Release();
-		virtual void SetPosition(const Vector2& position);
-		virtual void SetPosition(float x, float y);
-		virtual void SetPositionX(float x);
-		virtual void SetPositionY(float y);
-		virtual void SetSize(const Vector2& size);
-		virtual void SetSize(float width, float height);
-		virtual void SetWidth(float width);
-		virtual void SetHeight(float height);
-		virtual void SetVisible(bool visible);
-	protected:
-		Vector2 m_Position;
-		Vector2 m_Size;
-		bool m_Visible;
 	};
 }
 
